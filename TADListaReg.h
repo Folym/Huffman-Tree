fa4828d5c86064e4d5358c70605f81921dbcceb3
Heitor Folim <94811303+Folym@users.noreply.github.com>
2022-11-20 01:41:11 -0300

    Update TADListaReg.h

--- TADListaReg.h
+++ TADListaReg.h
@@ -2,8 +2,8 @@
 struct lencReg
 {
 	struct lencReg *prox;
-	int frq,simb,codgh;
-	char palv[50];
+	int frq,simb;
+	char codgh[50],palv[50];
 };
 typedef struct lencReg LencReg;
 
@@ -12,13 +12,49 @@ void initLR(LencReg **LR)
 	*LR = NULL;
 }
 
-void insereLR(LencReg **LR,int frq,int simb,char palv[50])
+void buscaPorSimb(LencReg**LR, int simb, char frase[],int *j)
+{
+	char palv[50];
+	int i=0;
+	LencReg *aux;
+	aux = *LR;
+	while(aux!=NULL && aux->simb!=simb)
+		aux = aux -> prox;
+	if(aux!=NULL)
+	{
+		while(aux->palv[i]!='\0')
+		{
+			strcpy(palv,aux->palv);
+			frase[*j]=palv[i];
+			i++;
+			(*j)++;
+		}
+	}
+	frase[*j]='\0';
+}
+
+LencReg* buscaPorPalv(LencReg **LR,char palv[])
+{
+	LencReg *aux;
+	aux = *LR;
+	while(aux!=NULL && strcmp(aux->palv,palv)!=0)
+		aux = aux->prox;
+	if(aux==NULL)
+		return NULL;
+	else
+		return aux;
+}
+
+
+void insereLR(LencReg **LR,int frq,int simb,char palv[],char codgh[])
 {
 	LencReg *aux,*novo = (LencReg*)malloc(sizeof(LencReg));
 	novo->prox = NULL;
 	strcpy(novo->palv,palv);
 	novo->frq = frq;
 	novo->simb = simb;
+	if(codgh!=NULL)
+		strcpy(novo->codgh,codgh);
 	if(*LR==NULL)
 		*LR = novo;
 	else
@@ -30,7 +66,7 @@ void insereLR(LencReg **LR,int frq,int simb,char palv[50])
 	}
 }
 
-char acrsFrq(LencReg **LR,char palv[50])
+char acrsFrq(LencReg **LR,char palv[])
 {
 	LencReg *aux;
 	aux = *LR;
@@ -43,4 +79,3 @@ char acrsFrq(LencReg **LR,char palv[50])
 	}
 	return 0;
 }
-
